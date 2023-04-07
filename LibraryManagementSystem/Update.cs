2a68cc241a99cff3d4d25fe6a7c1db8cb82de547
SUBEDIDARPAN <DARPAN@KEICSOFT01.local>
2023-04-07 15:14:04 +0900

    some features are added

--- LibraryManagementSystem/Update.cs
+++ LibraryManagementSystem/Update.cs
@@ -1,17 +1,6 @@
-﻿using LibraryManagementSystem;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace LMS
 {
